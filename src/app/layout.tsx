463517eab14d5e9f39c8b753f9a6db3b9c946c01
dk <kudin.dima@gmail.com>
2026-05-17 01:04:08 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Poppins } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'CyberGuard | Awareness on Fake Apps and Websites',
+  description: 'Learn to identify phishing websites, fake apps, and digital scams with our interactive platform.',
+  openGraph: {
+    "title": "CyberGuard - Stay Secure Online",
+    "description": "Interactive platform to educate students and users about online fraud.",
+    "siteName": "CyberGuard"
+  },
+};
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${poppins.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
